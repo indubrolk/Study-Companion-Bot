28afaf5e9ee7b9c856b89a1ea753e3577cb7ccb2
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-07-27 15:07:52 +0530

    update Timer.jsx

--- frontend/studybot/src/components/Timer.jsx
+++ frontend/studybot/src/components/Timer.jsx
@@ -6,12 +6,46 @@ function Timer() {
     const [mode, setMode] = useState("focus");
     const [cycles, setCycles] = useState(0);
 
-    useEffect{() => {
+    useEffect(() => {
         let timer;
         if (isRunning){
-
+            timer = setInterval(() => {
+                setTime((prev) => {
+                    if (prev === 1){
+                        handleTimerEnd();
+                        return 0;
+                    }
+                    return prev - 1;
+                });
+            }, 1000);
         }
-    }}
+        return () => clearInterval(timer);
+    }, [isRunning]);
+
+        const handleTimerEnd = () => {
+            setIsRunning(false);
+            if(mode === "focus"){
+                setMode("break");
+                setTime(5 * 60);
+            } else{
+                setMode("focus");
+                setTime(25 * 60);
+                setCycles((c) => c + 1);
+            }
+        };
+
+        const formatTime = () => {
+            const mins = Math.floor(time / 60);
+            .toString()
+                .padStart(2,"0");
+
+            const secs = (time % 60).toString().padStart(2, "0");
+            return `${mins}:${secs}`;
+        };
+
+        const handle
+
+    )
 }
 
 export default Timer;
