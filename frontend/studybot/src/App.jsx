c2996fc4273147e2091f025e446631f0d5649f55
Eshanhasitha <eshanhasitha5@gmail.com>
2025-08-04 02:59:56 +0530

    Changed a route

--- frontend/studybot/src/App.jsx
+++ frontend/studybot/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <>
       <Navbar/>
       <Routes>
-        <Route path="/" element={<App />} />
+      
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/about" element={<About />} />
       </Routes>
