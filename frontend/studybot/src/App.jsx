fc16fbb430ed849f74dc5b3ff029773322cee0e6
Eshanhasitha <eshanhasitha5@gmail.com>
2025-08-03 12:21:14 +0530

    Modified

--- frontend/studybot/src/App.jsx
+++ frontend/studybot/src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Navbar/>
+ 
      
     </>
   )
