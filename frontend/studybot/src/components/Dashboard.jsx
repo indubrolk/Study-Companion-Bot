8101670281a7022359774537aac8b400b53be8b5
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-08-04 15:18:09 +0530

    fixed tailwind css in Timer.jsx

--- frontend/studybot/src/components/Dashboard.jsx
+++ frontend/studybot/src/components/Dashboard.jsx
@@ -3,7 +3,7 @@ import Timer from './Timer'
 
 function Dashboard() {
   return (
-      <div className="">
+      <div className="grid h-56 grid-cols-3 content-start gap-4">
     <Timer/>
       </div>
 
