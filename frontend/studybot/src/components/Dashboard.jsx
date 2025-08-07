7e348ef172aad158d934b43885d68e87ef15b35a
Kavindu <wkanjana1590@gmail.com>
2025-08-07 17:37:49 +0530

    summarizer

--- frontend/studybot/src/components/Dashboard.jsx
+++ frontend/studybot/src/components/Dashboard.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ChatbotComponent from './ChatBot/ChatBot'
 import Timer from './Timer'
+import Summary from './summary'
 
 function Dashboard() {
   return (
     <div>
       <ChatbotComponent/>
-      <div className="grid h-56 grid-cols-3 content-start gap-4">
+      <div className="grid h-56 w-auto grid-cols-2 content-start gap-4">
       <Timer/>
+      <Summary />
       </div>
     </div>
     
