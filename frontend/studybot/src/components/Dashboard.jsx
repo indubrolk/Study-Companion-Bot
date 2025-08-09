d69e7d52114c37b1ea5cf4d05ce69d802819c562
Eshanhasitha <eshanhasitha5@gmail.com>
2025-08-09 11:08:59 +0530

    Changed the ChatBot

--- frontend/studybot/src/components/Dashboard.jsx
+++ frontend/studybot/src/components/Dashboard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import ChatbotComponent from './ChatBot/ChatBot'
 import Timer from './Timer'
+import LearningAssistant from './ChatBot/ChatBot'
 
 function Dashboard() {
   return (
     <div>
-      <ChatbotComponent/>
       <div className="grid h-56 grid-cols-3 content-start gap-4">
       <Timer/>
+      <LearningAssistant/>
       </div>
     </div>
     
