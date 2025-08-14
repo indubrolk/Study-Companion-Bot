7ca7abde904090c80a9fc7b586d1c46559aa0774
Eshanhasitha <eshanhasitha5@gmail.com>
2025-08-15 00:26:31 +0530

    Modified Dashboard.jsx

--- frontend/studybot/src/components/Dashboard.jsx
+++ frontend/studybot/src/components/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Timer from './Timer'
+import ChatBot from './ChatBot/ChatBot'
 import Quiz from './Quiz'
 import Summary from './summary'
 
@@ -11,6 +12,7 @@ function Dashboard() {
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 content-start sm:h-56 h-auto">
       <Timer/>
       <Summary />
+      <ChatBot/>
       <Quiz/>
       </div>
     </div>
